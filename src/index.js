0a6703341e6835585bedd09ebd573e27bdabfb47
Anubhav Vishwakarma <anubhavvishwakarma17@gmail.com>
2025-06-07 13:56:19 +0530

    util code discussion

--- src/index.js
+++ src/index.js
@@ -5,7 +5,18 @@ dotenv.config({
   path: './env'
 })
 connectDB()
-
+.then(() => {
+  app.on("error",(error) => {
+      console.log("Err",error);
+      throw error 
+    })
+  app.listen(process.env.PORT || 8000, () => {
+    console.log(`Server is running at port: ${process.env.PORT}`)
+  })
+})
+.catch((err) => {
+  console.log("MONGODB connection failed !!!",err);
+})
 
 
 
---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,14 @@
+class ApiResponse{
+  constructor(statusCode,data,message = "Success"){
+    this.statusCode = statusCode
+    this.data = data
+    this.message = message
+    this.success = statusCode < 400
+    //sever has statusCode
+    //for informational responses we have (100 to 400)
+    //for successful responses (200 to 299)
+    //Redirectin messages(300 to 399)
+    //client error responses(400 to 499)
+    //Server error responses(500 to 599)
+  }
+}
